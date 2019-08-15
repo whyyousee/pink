4ddecab295d91acd4969bbf4c6fb250a66244f0b
whyyousee <49287000+whyyousee@users.noreply.github.com>
2019-08-15 23:16:27 +0600

    .mobileMenu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
 
+	$('.js-toggleMenu').on('click', () => {
+    	$('.mobileMenu').fadeToggle(300);
+    	$('.header-menu-mobile').fadeIn(300);
+    	$('header').toggleClass('headerActive');
+    	$(".header-menu-mobile").toggleClass("header-menu-close");
+  	});
+
+
 	$('.js-slick-reviews').slick({
 	  arrows: false,
 	  dots: true,

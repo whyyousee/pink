573ad48ad44d543c268b0f5ea3993272f20c715a
whyyousee <49287000+whyyousee@users.noreply.github.com>
2019-09-18 22:54:14 +0600

    маска телефона (вернуть required везде, допилить названия классов чекбоксов и радиобаттонов)

--- js/main.js
+++ js/main.js
@@ -24,26 +24,31 @@ $(document).ready(function () {
 	  initialSlide: 1
 	});
 
+	$('input[type="tel"]').inputmask({mask: "+7 (999) 999-9999"});
+
+
 	$.validator.methods.email = function(value, element) {
         return this.optional(element) || /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-z]+/.test(value);
 	};
+
 	$.validator.methods.tel = function(value, element) {
         return this.optional(element) || /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/.test(value);
 	};
-	$('.js-validForm').each(function() {
-		var self = $(this);
 
-		self.validate({
-			errorPlacement: function(error, element) {
-				return false;
-			},
-			submitHandler: function() {
-				alert('Форма отправлена.');
-			}
+		$('.js-validForm').validate({
+		  rules: {
+		    userEmail: {
+		    email: true,
+		    required: true
+		    }
+		  },
+		  messages: {
+		    userEmail: {
+		    email: "Please enter your email",
+		    required: "*"
+		    }
+		  }
 		});
-	});
-
-	$('input[type="tel"]').inputmask({mask: "+7 (999) 999-9999"});
 
 });
 
